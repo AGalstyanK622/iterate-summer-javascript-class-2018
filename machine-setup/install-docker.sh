9102d628da2c581dba9605fbd4e9145ba97c3b1b
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-06-13 11:04:01 -0700

    Add Docker-Compose installation

--- machine-setup/install-docker.sh
+++ machine-setup/install-docker.sh
@@ -2,11 +2,20 @@
 
 set -e -u
 
-printf 'This script will restart the computer at the end\n'
+printf 'This script is meant to be run on Debian/Ubuntuy && will restart the computer at the end\n'
 
 apt-get update
-apt-get install -qq -y tig git
+apt-get install -qq -y tig git curl
 
 curl -fsSL get.docker.com | bash -
 usermod -a -G docker $USER
+
+# Now to get docker-compose
+
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) \
+     -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+# Reboot is for the group ID to take affect
 reboot
